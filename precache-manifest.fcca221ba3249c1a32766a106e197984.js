265456815aa409389fbe386b9729a91d1816c28e
Jonas Holbech <jh@jonasholbech.dk>
2020-10-18 15:17:26 +0200

    Updates

---
+++ precache-manifest.fcca221ba3249c1a32766a106e197984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91d58868d7835215407bacb96e8323c",
+    "url": "/stopwatch-react/index.html"
+  },
+  {
+    "revision": "0f4b9fdba811dc1acc18",
+    "url": "/stopwatch-react/static/css/main.e3f8a2a6.chunk.css"
+  },
+  {
+    "revision": "51283bd5cfee31df4f65",
+    "url": "/stopwatch-react/static/js/2.924c1f26.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/stopwatch-react/static/js/2.924c1f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4b9fdba811dc1acc18",
+    "url": "/stopwatch-react/static/js/main.ea908b01.chunk.js"
+  },
+  {
+    "revision": "c2dc1705f8b9b9b4e289",
+    "url": "/stopwatch-react/static/js/runtime-main.ea94b2db.js"
+  }
+]);
